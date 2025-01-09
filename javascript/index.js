d6c2aeed8fdec35578cf8aec4f4c98844aaf8423
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-09 08:36:43 +0530

    DOM-2

--- javascript/index.js
+++ javascript/index.js
@@ -1,86 +1,32 @@
 /**
- * DOM manipulations
- * -----------------
- *   To manipulate our elements , 
- *   every tag is an object 
- *      
- *      document
- *          body
- *          documentElement
- *          title
- *          head
- *          links
- *          images
- *          URL
- *          forms
+ *  DOM
+ * ----
+ *  document.body
+ *  document.images
+ *  document.links
+ *  document.head , title , forms , URL 
  */
-console.log(window);
-console.log(window.document.documentElement);
-console.log(window.document.body);
-console.log(window.document.title);
-window.document.title = "DOM"
-console.log(window.document.head);
-console.log(document.title);
-var body = document.body;
-console.log(body);
-var links = document.links;
-console.log(links);
-var images = document.images;
 
-console.clear();
-// var div = document.createElement("div");
-// div.innerText = "Created My First Element Using createElement()";
+var div1 = document.getElementById("text-1");
+div1.innerText = "<b>Javascript</b>";
 
-// document.body.appendChild(div);
+var div2 = document.getElementById("text-2");
+div2.innerHTML = "<b style='color : red'>Javascript</b>"
+console.log(div2.innerHTML);
+console.log(div2);
 
-// console.log(div.innerText);
-// console.log(div.innerHTML);
+var flag = window.confirm("Do you really want to delete ?");
+console.log(flag);
 
-// console.log(document.body.innerText);
-// console.log(document.body.innerHTML);
+if (flag) {
+    div2.remove();
+}
 
-// var div = document.createElement("div");
-// div.className = "container";
-// console.log(div);
 
-// var img = document.createElement("img");
-// img.src = '../Images/img4.jpg';
-
-// var h2 = document.createElement("h2");
-// h2.innerText = "Cat Image";
-
-// var p = document.createElement("p");
-// p.innerText = "Domestic cats are characterized by retractable claws, powerful bodies, acute senses, long tails, and specialized teeth adapted for hunting prey. The following article deals with general characteristics of the domestic cat. For information on larger cats, see their individual entries (such as lion, tiger, and leopard).";
-
-// div.append(img, h2, p);
-// // div.appendChild(img);
-// // div.appendChild(h2);
-// // div.appendChild(p);
-
-// document.body.appendChild(div);
-
-var header = document.createElement("header");
-
-var img = document.createElement("img");
-img.src = '../Images/img4.jpg';
-
-var item = document.createElement("div");
-item.className = "item";
-
-var a1 = document.createElement("a");
-a1.innerText = "Menu";
-a1.href = "#"
-
-var a2 = document.createElement("a");
-a2.innerText = "About";
-a2.href = "#"
-
-var a3 = document.createElement("a");
-a3.innerText = "Contact";
-a3.href = "#"
-
-item.append(a1, a2, a3);
-
-header.append(img, item);
-
-document.body.appendChild(header);
+/**
+ * if(condition){
+ * 
+ * }else{
+ *      
+ * }
+ */
