e0d7c62c84694daabaeb57bc1fb8a91751ea7121
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-09 09:08:34 +0530

    DOM - 3

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,12 @@
-/**
- *  DOM
- * ----
- *  document.body
- *  document.images
- *  document.links
- *  document.head , title , forms , URL 
- */
-
-var div1 = document.getElementById("text-1");
-div1.innerText = "<b>Javascript</b>";
-
-var div2 = document.getElementById("text-2");
-div2.innerHTML = "<b style='color : red'>Javascript</b>"
-console.log(div2.innerHTML);
-console.log(div2);
-
-var flag = window.confirm("Do you really want to delete ?");
-console.log(flag);
-
-if (flag) {
-    div2.remove();
+function callhim() {
+    var container = document.getElementById("container");
+    container.style.background = "yellow";
+    document.body.style.background = "red";
 }
 
-
-/**
- * if(condition){
- * 
- * }else{
- *      
- * }
- */
+function deletehim() {
+    var container = document.getElementById("container");
+    // container.remove();
+    // container.style.display = "none";
+    container.style.opacity = "0";
+}
