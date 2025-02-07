8353a1f4a2a473e691c38390bf2a0c767d78ca06
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-02-07 15:57:42 +0530

    Fetch With Items

--- javascript/index.js
+++ javascript/index.js
@@ -1,43 +1,20 @@
-let table = document.getElementById("table");
 async function getData() {
     let response = await fetch("https://fakestoreapi.com/products");
     let data = await response.json();
     displayData(data);
 }
 function displayData(data) {
-    createHeader(data[0]);
-    let tbody = document.createElement("tbody");
+    let container = document.getElementById("container");
     data.forEach(obj => {
-        let tr = document.createElement("tr");
-        for (let key in obj) {
-            if (key !== "rating") {
-                let td = document.createElement("td");
-                if (key === "image") {
-                    let img = document.createElement("img");
-                    img.src = obj[key];
-                    td.appendChild(img);
-                } else {
-                    td.innerText = obj[key];
-                }
-                tr.appendChild(td);
-            }
-        }
-        tbody.appendChild(tr);
+        let item = document.createElement("div");
+        item.className = "item";
+        item.innerHTML = `
+            <div class="image-container"> <img src = '${obj["image"]}'> </div>
+            <p>${obj["title"]}</p>
+            <p>${obj["category"]}</p>
+            <div class='text-container'>${obj["description"]}</div>
+        `
+        container.appendChild(item);
     });
-    table.appendChild(tbody);
 }
-function createHeader(obj) {
-    let thead = document.createElement("thead");
-    let theadRow = document.createElement("tr");
-    for (let key in obj) {
-        if (key !== "rating") {
-            let th = document.createElement("th");
-            th.innerText = key;
-            theadRow.appendChild(th);
-        }
-    }
-    thead.appendChild(theadRow);
-    table.appendChild(thead);
-}
-
 getData();
