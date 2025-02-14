72d59ded83753f19f3e7663694c93fb4175b64bc
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-02-14 15:49:48 +0530

    See More

--- javascript/index.js
+++ javascript/index.js
@@ -1,71 +1,25 @@
 let container = document.getElementById("container");
-let btnContainer = document.getElementById("btn-container");
 
 async function getData() {
-    try {
-        let response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) {
-            throw new Error(`HTTP Request : ${response.statusText}`);
-        }
-        let result = await response.json();
-        localStorage.setItem("products", JSON.stringify(result));
-        createButton();
-        displayData();
-    } catch (err) {
-        console.error(err);
-    }
+    let response = await fetch("https://fakestoreapi.com/products");
+    let products = await response.json();
+    localStorage.setItem("products", JSON.stringify(products));
+    displayData();
 }
-function createButton() {
-    btnContainer.innerHTML = ``;
-    let products = JSON.parse(localStorage.getItem("products")) || [];
-    if (products.length !== 0) {
-        let result = Array.from(new Set(products.map(obj => obj.category)));
-        result.forEach(ele => {
-            let button = document.createElement("button");
-            button.className = "btn btn-outline-secondary px-3 py-2 fs-3";
-            button.textContent = ele;
-            button.addEventListener("click", () => {
-                filterData(ele);
-            })
-            btnContainer.appendChild(button);
-        })
-    }
+function displayData() {
+    let products = JSON.parse(localStorage.getItem("products"));
+    products.forEach(obj => {
+        let item = document.createElement("div");
+        item.innerHTML = `
+            <img src=${obj.image}>
+            <p>${obj.title}</p>
+            <button onclick='showProduct(${obj.id})'>See More</button>
+        `
+        container.appendChild(item);
+    })
 }
-function filterData(category) {
-    let products = JSON.parse(localStorage.getItem("products")) || [];
-    if (products.length !== 0) {
-        let result = products.filter(obj => obj.category === category);
-        displayData(result);
-    }
+function showProduct(id) {
+    window.location.href = `./product.html?id=${id}`;
 }
 
-function displayData(filterProducts) {
-    container.innerHTML = ``;
-    let products = JSON.parse(localStorage.getItem("products")) || [];
-    if (filterProducts !== undefined) {
-        products = filterProducts;
-    }
-    if (products.length === 0) {
-        container.innerHTML = "No data Available";
-    } else {
-        products.forEach(obj => {
-            let item = document.createElement("div");
-            item.className = "border border-3 border-warning m-2 p-2 d-flex gap-2 align-items-center    ";
-            item.innerHTML = `
-                <div>
-                    <img class='w-100 object-fit-contain' style='height : 250px' src=${obj.image}>
-                </div>
-                <div>
-                    <h3 class="text-center">Title : ${obj.title}</h3>
-                <p class="mb-0">Description : ${obj.description}</p>
-                <p class="fw-bold">Category : ${obj.category}</p>
-                </div>
-            `;
-            container.appendChild(item);
-        })
-    }
-}
-
-
-
-window.onload = getData;
+getData();
