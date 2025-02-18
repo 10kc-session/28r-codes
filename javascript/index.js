12a65941a6e7d524d86a9d7e6bc3684667256593
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-02-18 16:41:24 +0530

    Object Methods

--- javascript/index.js
+++ javascript/index.js
@@ -1,111 +1,165 @@
-// function Dog(name, breed, age) {
-//     this.name = name;
-//     this.breed = breed;
-//     this.age = age;
-//     this.getData = function () {
-//         console.log(this.name, this.breed, this.age)
-//     }
-// }
-// let dog1 = new Dog("Pinky", "indie", 3);
-// // dog1.getData();
-
-// let dog2 = new Dog("blacky", "German", 1);
-// // dog2.getData();
-
-// function printObjData(salary, address) {
-//     console.log(this);
-//     console.log(`Employee Name is ${this.empName} and His Department is ${this.empDept}
-// and salary is ${salary} and address is ${address}`);
-// }
-// let obj = {
-//     "empName": "Srinivas",
-//     "empDept": "Software"
-// }
-// // printObjData.call(obj, 20000, 'hyderabad');
-// // printObjData.apply(obj, [20000, 'hyderabad']);
-// result = printObjData.bind(obj);
-// result(20000, 'hyderabad');
-
-
-// // let obj1 = {
-// //     "empName": "Rajesh",
-// //     "empDept": "Software",
-// //     "balance": "50000",
-// //     "getBalance": function () {
-// //         console.log(this.balance);
-// //     }
-// // }
-// // let obj2 = {
-// //     "empName": "Ravi Kumar",
-// //     "empDept": "Hardware",
-// //     "balance": "45000"
-// // }
-
-// // obj1.getBalance.call(obj2);
-// // obj1.getBalance.apply(obj2);
-// // obj1.getBalance.bind(obj2)();
-
-// // printObjData.call(obj1);
-// // printObjData.apply(obj1);
-// // printObjData.bind(obj1)();
-// // call , apply and bind
-
-let container = document.createElement("div");
-
-async function getData() {
-    let response = await fetch("https://jsonplaceholder.typicode.com/albums", { method: "GET" });
-    let result = await response.json();
-    localStorage.setItem("albums", JSON.stringify(result));
-    displayData();
+/**
+ * Object Methods in Javascript
+ * ----------------------------
+ *      keys , values , create , entries , seal , freeze , assign 
+ */
+let obj1 = {
+    "empName": "Vijay",
+    "empDept": "SE"
 }
-function displayData() {
-    container.innerHTML = ``;
-    container.classList.add('container');
-    let albums = JSON.parse(localStorage.getItem("albums")) || [];
-    if (albums.length > 0) {
-        albums.forEach((obj, index) => {
-            const { id, title } = obj;
-            let item = document.createElement("div");
-            item.innerHTML = `
-                <p><b>id:</b>${id}</p>
-                <p><b>title:</b>${title}</p>
-                <button onclick='extraData(${index})'>See More</button>
-                <button onclick = 'deleteData(${index})'>Delete</button>
-            `;
-            container.appendChild(item);
-        });
+let result = Object.values(obj1);
+console.log(result);
+
+const { empName, empDept } = obj1;
+console.log(empName, empDept);
+
+const [key1, key2] = Object.keys(obj1);
+console.log(key1, key2);
+
+result = Object.entries(obj1);
+console.log(result);
+
+for (let [key, value] of result) {
+    console.log(key + " : " + value);
+}
+console.clear();
+const obj2 = {
+    "animal": "dog",
+    "bread": "Indie"
+}
+// Object.seal(obj2);
+
+// allows reading and updating 
+
+obj2.animal = "Cat";
+obj2.food = "Wiskus";
+
+delete obj2.bread;
+
+console.log(obj2);
+
+console.clear();
+
+const obj3 = {
+    "animal": "dog",
+    "bread": "Indie"
+}
+
+Object.freeze(obj3);
+
+obj3.animal = "Kangaroo";
+delete obj3.animal
+
+obj3.food = "Milk";
+
+console.log(obj3);
+
+// console.log(Object.getOwnPropertyDescriptors(obj3));;
+
+console.clear();
+
+let obj = {};
+
+Object.defineProperties(obj, {
+    "name": {
+        value: "Hemanth",
+        enumerable: true,
+        writable: true,
+        configurable: true
+    },
+    "salary": {
+        value: "10k",
+        enumerable: false,
+        writable: true,
+        configurable: false
     }
-    document.body.appendChild(container);
+});
+
+
+
+obj.salary = "20k";
+obj.name = "Hemanth Billa"
+
+delete obj.name;
+
+console.log(obj.salary, obj.name);
+
+let obj5 = Object.defineProperty({}, "name", {
+    value: "XYZ",
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+console.log(obj5);
+
+
+let obj6 = {
+    "street": "KPHB",
+    "city": "Hyderabad"
+}
+console.log(Object.getOwnPropertyDescriptors(obj6));
+console.log(Object.getOwnPropertyDescriptor(obj6, "street"));
+
+Object.freeze(obj6);
+console.clear();
+console.log(Object.getOwnPropertyDescriptors(obj6));
+
+console.clear();
+
+let obj7 = {
+    "product": "Laptop",
+    "price": "35000",
+    "brand": "HP"
 }
 
-function deleteData(index) {
-    let albums = JSON.parse(localStorage.getItem("albums")) || [];
-    albums.splice(index, 1);
-    localStorage.setItem("albums", JSON.stringify(albums));
-    displayData();
+let obj8 = Object.create(obj7);
+console.log(obj8);
+console.log(obj8.product, obj8.price, obj8.brand);
+
+let obj9 = Object.create(obj7, {
+    "service": {
+        value: "Hyderabab",
+        enumerable: true,
+        writable: true,
+        configurable: true
+    },
+    "warranty": {
+        value: "2 Years",
+        enumerable: true,
+        writable: true,
+        configurable: true
+    }
+});
+console.log(obj9);
+
+
+console.clear();
+
+let obj10 = {
+    "name": "Rahul"
+}
+let obj11 = {
+    "salary": "30000"
 }
 
+let newObj = Object.assign(obj10, obj11);
 
-function extraData(index) {
-    // window.location.href = `./more.html?id=${index}`;
-    window.open(`./more.html?id=${index}`, "_blank");
+console.log(newObj);
+
+newObj.salary = "45000";
+
+console.log(obj10);
+
+let obj13 = {
+    "xyz": "123"
 }
 
+let obj12 = { ...obj13, ...obj11 };
+console.log(obj12);
 
+obj12.xyz = "456";
 
+console.log(obj13);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-getData();
+console.log(obj12);
