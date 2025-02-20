0576f54ed7faab0479dcf34742ed477f7758abeb
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-02-20 15:37:04 +0530

    Oops

--- javascript/index.js
+++ javascript/index.js
@@ -1,165 +1,195 @@
 /**
- * Object Methods in Javascript
- * ----------------------------
- *      keys , values , create , entries , seal , freeze , assign 
+ *   OOPS IN JS
+ *  --------------
+ *      classes
+ *           classes defines creation of object
+ *           classes are collections of properties and methods 
+ *           properties are state and methods are behviour of an object 
+ *           classes are introduced in ES6
+ *           there can be only one constructor in a class 
+ *              Syn :   
+ *                      class ClassName{
+ *                              // properties and methods
+ *                       }
+ *      objects
+ *      encapsulation
+ *      inheritance
+ *      polymorphism    
+ *      abstraction
  */
-let obj1 = {
-    "empName": "Vijay",
-    "empDept": "SE"
-}
-let result = Object.values(obj1);
-console.log(result);
+// function Car(name, engine) {
+//     this.name = name;
+//     this.engine = engine;
+// }
+// Car.prototype.getDetails = function () {
+//     console.log(`Car Name is ${this.name} and Engine Type is ${this.engine}`);
+// }
+// let car = new Car("Volkswagen", "1.5TSI");
+// car.getDetails();
 
-const { empName, empDept } = obj1;
-console.log(empName, empDept);
+// let car2 = new Car("Skoda", '1.0TSI');
+// car2.getDetails();
 
-const [key1, key2] = Object.keys(obj1);
-console.log(key1, key2);
-
-result = Object.entries(obj1);
-console.log(result);
-
-for (let [key, value] of result) {
-    console.log(key + " : " + value);
-}
-console.clear();
-const obj2 = {
-    "animal": "dog",
-    "bread": "Indie"
-}
-// Object.seal(obj2);
-
-// allows reading and updating 
-
-obj2.animal = "Cat";
-obj2.food = "Wiskus";
-
-delete obj2.bread;
-
-console.log(obj2);
-
-console.clear();
-
-const obj3 = {
-    "animal": "dog",
-    "bread": "Indie"
-}
-
-Object.freeze(obj3);
-
-obj3.animal = "Kangaroo";
-delete obj3.animal
-
-obj3.food = "Milk";
-
-console.log(obj3);
-
-// console.log(Object.getOwnPropertyDescriptors(obj3));;
-
-console.clear();
-
-let obj = {};
-
-Object.defineProperties(obj, {
-    "name": {
-        value: "Hemanth",
-        enumerable: true,
-        writable: true,
-        configurable: true
-    },
-    "salary": {
-        value: "10k",
-        enumerable: false,
-        writable: true,
-        configurable: false
+class Car {
+    // properties 
+    name;
+    engine;
+    // use to assing values to properties
+    constructor(name, engine) {
+        this.name = name;
+        this.engine = engine;
     }
-});
-
-
-
-obj.salary = "20k";
-obj.name = "Hemanth Billa"
-
-delete obj.name;
-
-console.log(obj.salary, obj.name);
-
-let obj5 = Object.defineProperty({}, "name", {
-    value: "XYZ",
-    writable: true,
-    enumerable: true,
-    configurable: true
-});
-
-console.log(obj5);
-
-
-let obj6 = {
-    "street": "KPHB",
-    "city": "Hyderabad"
-}
-console.log(Object.getOwnPropertyDescriptors(obj6));
-console.log(Object.getOwnPropertyDescriptor(obj6, "street"));
-
-Object.freeze(obj6);
-console.clear();
-console.log(Object.getOwnPropertyDescriptors(obj6));
-
-console.clear();
-
-let obj7 = {
-    "product": "Laptop",
-    "price": "35000",
-    "brand": "HP"
-}
-
-let obj8 = Object.create(obj7);
-console.log(obj8);
-console.log(obj8.product, obj8.price, obj8.brand);
-
-let obj9 = Object.create(obj7, {
-    "service": {
-        value: "Hyderabab",
-        enumerable: true,
-        writable: true,
-        configurable: true
-    },
-    "warranty": {
-        value: "2 Years",
-        enumerable: true,
-        writable: true,
-        configurable: true
+    // method
+    getDetails() {
+        console.log(`Car Name is ${this.name} and Engine Type is ${this.engine}`);
     }
-});
-console.log(obj9);
+}
+
+let car1 = new Car("Volkswagen", "1.5TSI");
+car1.getDetails();
+console.log(car1.name);
+console.log(car1.engine);
 
 
+class Student {
+    studentName;
+    studentId;
+    studentBranch;
+
+    constructor(studentName, studentId, studentBranch) {
+        this.studentBranch = studentBranch;
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    coding() {
+        console.log(`${this.studentName} is coding javascript or python`);
+    }
+
+    playing() {
+        console.log(`The Student With Id : ${studentObj1.studentId} is playing cards`);
+    }
+
+    drinkTeaOrCoffee() {
+        console.log(`Baga alisipoyi tea tagutunna ${this.studentName}`);
+    }
+
+}
+
+let studentObj1 = new Student("Bharath", 101, "ML and AI");
+let studentObj2 = new Student("Anand", 102, "CSE");
+let studentObj3 = new Student("Ishawarya", 103, "AI and ML");
+studentObj3.studentName = "Ishwarya";
+studentObj1.coding();
+studentObj3.drinkTeaOrCoffee();
+studentObj2.playing();
+
+
+// Inheritance
+
+class Animal {
+    eyes;
+    color;
+    sounds;
+    legs;
+    tail;
+    constructor(eyes, color, sounds, legs, tail) {
+        this.eyes = eyes;
+        this.color = color;
+        this.sounds = sounds;
+        this.legs = legs;
+        this.tail = tail;
+    }
+    eating() {
+        console.log("Animal is Eating");
+    }
+    sleeping() {
+        console.log("Animal is Sleeping");
+    }
+    hunting() {
+        console.log("Animal is hunting");
+    }
+}
+
+class Cat extends Animal {
+    name;
+    food;
+    breed;
+    constructor(eyes, color, sounds, legs, tail, food, breed, name) {
+        super(eyes, color, sounds, legs, tail);
+        this.food = food;
+        this.breed = breed;
+        this.name = name;
+    }
+    eating() {
+        console.log(`${this.name} is eating ${this.food}`);
+    }
+    sleeping() {
+        console.log(`${this.name} is sleeping for 16 hours`);
+    }
+    hunting() {
+        console.log(`${this.name} is hunting for cat food`);
+    }
+}
 console.clear();
+let cat = new Cat("black color eyes",
+    "white", "meow", "4 legs", "available", "cat food", "bengal cat", "ino");
 
-let obj10 = {
-    "name": "Rahul"
+console.log(cat);
+cat.sleeping();
+cat.eating();
+cat.hunting();
+
+class Dog extends Animal {
+    name;
+    food;
+    breed;
+    constructor(eyes, color, sounds, legs, tail, food, breed, name) {
+        super(eyes, color, sounds, legs, tail);
+        this.food = food;
+        this.breed = breed;
+        this.name = name;
+    }
+    eating() {
+        console.log(`${this.name} is eating ${this.food}`);
+    }
+    sleeping() {
+        console.log(`${this.name} is sleeps 8 hours`);
+    }
+    hunting() {
+        console.log(`${this.name} is hunting for dog food`);
+    }
 }
-let obj11 = {
-    "salary": "30000"
+
+let dog1 = new Dog("brown color", "brown", "bow bow",
+    "4 legs", "available", "bones", "husky", "chimtu");
+console.log(dog1)
+dog1.eating();
+dog1.sleeping();
+dog1.hunting();
+
+
+// Encapsulation
+
+class Employee {
+    #employeeName;
+    #employeeId;
+    #employeeSalary;
+    constructor(employeeId, employeeName, employeeSalary) {
+        this.#employeeId = employeeId;
+        this.#employeeName = employeeName;
+        this.#employeeSalary = employeeSalary;
+    }
+    get getEmployeeName() {
+        return this.#employeeName;
+    }
+    set setEmployeeName(name) {
+        this.#employeeName = name;
+    }
 }
-
-let newObj = Object.assign(obj10, obj11);
-
-console.log(newObj);
-
-newObj.salary = "45000";
-
-console.log(obj10);
-
-let obj13 = {
-    "xyz": "123"
-}
-
-let obj12 = { ...obj13, ...obj11 };
-console.log(obj12);
-
-obj12.xyz = "456";
-
-console.log(obj13);
-
-console.log(obj12);
+console.clear();
+let tcsHr = new Employee(100, "Raghu", 20000);
+console.log(tcsHr); // dell hr
+console.log(tcsHr.getEmployeeName);
+tcsHr.setEmployeeName = "Raghu Krishna";
+console.log(tcsHr.getEmployeeName);
